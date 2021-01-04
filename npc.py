0421657b397989a85bb4fc7cf36d033e7d5c62ae
Spandana Nallapati <drsnallapati@gmail.com>
2021-01-03 22:00:57 -0500

    made dialogue and npc disappear after 3 seconds

--- npc.py
+++ npc.py
@@ -28,7 +28,6 @@ class NPC(pygame.sprite.Sprite):
         self.surf = self.level_one_NPC_surface
         self.rect = self.level_one_NPC_surface.get_rect(topleft=[x, y])
         # --- dialogue box ---
-        # Create surface to draw dialog on to
         self.font = pygame.font.SysFont("monospace", 20)
         self.dialogue1_text = self.font.render("Ah! I see you were able to find your way!", True, pygame.Color("white"), pygame.Color("black"))
         self.dialogue1_textRect = self.dialogue1_text.get_rect(
@@ -39,21 +38,30 @@ class NPC(pygame.sprite.Sprite):
             center=[self.screen_width / 2, ((self.screen_height/2) + (self.dialogue1_text.get_height()+14))]
         )
         self.active_NPC = False
+        self.show_dialogue = False
 
     def update(self):
-        if self.rect.colliderect(self.player.rect):
+        if self.rect.colliderect(self.player.rect) and self.active_NPC == False:
             self.active_NPC = True
+            self.show_dialogue = True
+            self.start_ticks = pygame.time.get_ticks() #starter tick
+        if self.active_NPC == True:
+            seconds = (pygame.time.get_ticks()-self.start_ticks)/1000 #calculate how many seconds
+            if seconds > 4: #if more than 4 seconds close the game
+                self.show_dialogue = False #remove dialogue and remove NPC surfs
+                self.kill()
 
     def draw_after_clipping(self):
-            if self.active_NPC == True:
+            if self.show_dialogue == True:
                 self.screen.blit(self.dialogue1_text,self.dialogue1_textRect)
                 self.screen.blit(self.dialogue2_text,self.dialogue2_textRect)
 
+
+# ----TODO FINAL BOSS FIGHT -----
+# boss is impassable
 # when you jump down from the block, it should trigger a dialogue box
-# Dialogue box whole text at once and pressing enter key moves the dialogue along
 # at the end of dialogue box, the fight begins
 # your circle gets twice as big
-# boss is impassable
 # Level one boss shoots explosions in a outward circular pattern
 # Boss life bar is shown at the bottom of the screen - updates with each hit
 # half way - pattern becomes more dense and faster
